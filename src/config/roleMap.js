4e62fd551a11b70a78125656b6360034601bb80e
Varsha Mewada <mewadavarsha70@gmail.com>
2026-03-30 14:08:45 +0530

    Add whatsapp module

---
+++ src/config/roleMap.js
@@ -0,0 +1,45 @@
+const ROLE_MAP = {
+  "FRONT OFFICE": [
+    "F.O.M",
+    "F.O.A",
+    "B.Boy",
+  ],
+
+  "F&B SERVICE": [
+    "Bar Captain",
+    "Trainee Captain",
+    "Sr Steward",
+    "Steward",
+  ],
+
+  "KITCHEN": [
+    "Helper",
+    "Tandoor",
+  ],
+
+  "UTILITY": [
+    "Utility",
+  ],
+
+  "GUARD": [
+    "Guard",
+  ],
+
+  "STORE": [
+    "Store",
+  ],
+
+  "HOUSEKEEPING": [
+    "Room Attendant",
+    "Room Boy",
+    "Linen Man",
+  ],
+
+  "MAINTENANCE": [
+    "Electrician",
+    "Supervisor",
+    "Maintenance",
+  ],
+};
+
+module.exports = ROLE_MAP;
