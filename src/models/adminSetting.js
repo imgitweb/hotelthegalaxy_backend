f2ebfadb7c2984ed6cea176d456d318e4eb83337
Varsha Mewada <mewadavarsha70@gmail.com>
2026-04-07 15:02:55 +0530

    updated

---
+++ src/models/adminSetting.js
@@ -0,0 +1,16 @@
+// models/settings.model.js
+const mongoose = require("mongoose");
+
+const settingsSchema = new mongoose.Schema(
+  {
+    hotelName: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String },
+    location: { type: String },
+    description: { type: String },
+    avatar: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Settings", settingsSchema);
